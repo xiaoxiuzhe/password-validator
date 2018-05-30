f3948e67a7e9583c7bc2d6c24864489de277b685
XIUZHE\xiuzhe <xiaoxiuzhe@163.com>
2018-05-30 19:57:56 -0300

    create an UI that will perform the password validation

--- app/src/main/java/ca/dal/password_validator/MainActivity.java
+++ app/src/main/java/ca/dal/password_validator/MainActivity.java
@@ -2,6 +2,9 @@ package ca.dal.password_validator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonOnClick(View v){
+        TextView textView = (TextView) findViewById(R.id.textView);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        Validator validator = new Validator();
+        if(validator.validate(editText.getText().toString())){
+            textView.setText("Password is strong enough");
+        }
+        else{
+            textView.setText(validator.printMessage());
+        }
     }
 }
