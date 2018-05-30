a4b48e0bcf4d5592539f19bb17d01046fda98a3c
XIUZHE\xiuzhe <xiaoxiuzhe@163.com>
2018-05-30 00:18:51 -0300

    modify validate method to make new tests pass

--- app/src/main/java/ca/dal/password_validator/Validator.java
+++ app/src/main/java/ca/dal/password_validator/Validator.java
@@ -7,10 +7,28 @@ public class Validator {
     public boolean validate(String password){
         valid = true;
         this.password = password;
+        //password must have at least 8 characters
         if (password.length() < 8){
             valid = false;
         }
+        //password must no be "password"  (case insensitive)
         if (password.toLowerCase().equals("password")){
+            valid = false;
+        }
+        //password must have a uppercase character
+        if (password.equals(password.toLowerCase())){
+            valid = false;
+        }
+        //password must have a lowercase character
+        if (password.equals(password.toUpperCase())){
+            valid = false;
+        }
+        //at least one digit is required
+        if (!password.matches(".*\\d+.*")){
+            valid = false;
+        }
+        //at least one special character is required
+        if (password.matches("[a-zA-Z0-9 ]*")){
             valid = false;
         }
         return valid;
